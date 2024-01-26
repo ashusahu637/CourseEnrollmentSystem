0cc31b358fc55ca767f63de828c1e6d73ae88433
Ashutosh <ashusahu637@gmail.com>
2024-01-26 18:07:36 +0530

    modified main

--- crs/src/main/java/com/courseEnrollmentSystem/crs/CrsApplication.java
+++ crs/src/main/java/com/courseEnrollmentSystem/crs/CrsApplication.java
@@ -9,13 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.courseEnrollmentSystem.crs.classes")
-@EnableJpaRepositories(basePackages = "com.courseEnrollmentSystem.crs.repositories")
+@EnableJpaRepositories(basePackages ="com.courseEnrollmentSystem.crs.repositories")
 public class CrsApplication {
 
-	
 	public static void main(String[] args) {
-		SpringApplication.run(CrsApplication.class, args);
 		
+		SpringApplication.run(CrsApplication.class, args);
 		
 	}
 
